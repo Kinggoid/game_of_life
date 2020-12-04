c025503ce99c92b5c60ea9b916bd0059a71ee594
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2020-12-04 17:27:43 +0100

    Update test_simulator.py
    test regel twee weer opnieuw

--- test_simulator.py
+++ test_simulator.py
@@ -61,3 +61,21 @@ class TestSimulator(TestCase):
         self.sim.world.set(x, y)
         self.sim.update()
         self.assertEqual(self.sim.world.get(x, y), 0)
+
+    def test_over_population(self):
+        """
+        Checks how many neighbours a cel has. If it has less than two, it will die.
+        """
+
+        # Make a small world.
+        world = World(10, 10)
+        self.sim.set_world(world)
+
+        # Create a cell and a few surrounding neighbours
+        coordinates = [[2,5], [3,5], [1,5], [2,6], [2,7]]
+
+        for cell in coordinates:
+            self.sim.world.set(cell[0], cell[1])
+
+        self.sim.update()
+        self.assertEqual(self.sim.world.get(2, 5), 0)
