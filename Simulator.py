a77a55ce10ab9a74d7d4ca4acc9c7f68306e21f7
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2020-12-04 19:05:50 +0100

    Update Simulator.py
    You can now read the parameters from the world you made so you can get your new rules.

--- Simulator.py
+++ Simulator.py
@@ -1,15 +1,15 @@
 from World import *
 
+
 class Simulator:
     """
     Game of Life simulator. Handles the evolution of a Game of Life ``World``.
     Read https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for an introduction to Conway's Game of Life.
     """
 
-    def __init__(self, world = None):
+    def __init__(self, world=None):
         """
         Constructor for Game of Life simulator.
-
         :param world: (optional) environment used to simulate Game of Life.
         """
         self.generation = 0
@@ -21,35 +21,36 @@ class Simulator:
     def update(self) -> World:
         """
         Updates the state of the world to the next generation. Uses rules for evolution.
-
         :return: New state of the world.
         """
         self.generation += 1
+
+
 
         for x in range(0, self.world.width):
             for y in range(0, self.world.height):
                 neighbours = sum(self.world.get_neighbours(x, y))
 
-                if neighbours < 2:
+                parameters = self.world.get_parameters()
+
+                if neighbours < parameters[0]:
                     # This cell will die due to underpopulation
                     self.world.set(x, y, 0)
-                elif neighbours > 3:
+                elif neighbours > parameters[1]:
                     # This cell will die due to overpopulation
                     self.world.set(x, y, 0)
-                elif neighbours == 3:
+                elif neighbours == parameters[1]:
                     # This cell came to life (or if it allready was alive it will stay alive)
                     self.world.set(x, y, 1)
-                elif neighbours == 2:
+                elif neighbours == parameters[0]:
                     # This cell survived
                     pass
-                
 
         return self.world
 
     def get_generation(self):
         """
         Returns the value of the current generation of the simulated Game of Life.
-
         :return: generation of simulated Game of Life.
         """
         return self.generation
@@ -57,7 +58,6 @@ class Simulator:
     def get_world(self):
         """
         Returns the current version of the ``World``.
-
         :return: current state of the world.
         """
         return self.world
@@ -65,8 +65,6 @@ class Simulator:
     def set_world(self, world: World) -> None:
         """
         Changes the current world to the given value.
-
         :param world: new version of the world.
-
         """
         self.world = world
