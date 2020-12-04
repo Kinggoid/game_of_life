cd12dae3e751a7009989e0ad074429602e47a9a4
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2020-12-04 19:51:28 +0100

    Update World.py
    you can change the parameters at the beginning of the simulation

--- World.py
+++ World.py
@@ -1,79 +1,44 @@
-import numpy as np
-from typing import List
+from Visualisation import *
+from Simulator import *
+import time
 
-class World:
-    """
-    Data structure for representing Game of Life worlds.
-    """
+# Configuratie
+VISUALISATION=True
 
-    survive = [2, 3]
-    birth = [3]
+if __name__ == "__main__":
+    w = World(110)
+    sim = Simulator(w)
 
-    def __init__(self, width: int, height: int = -1):
-        """
-        Constructor of World datatype.
+    change_parameters = input(
+        'Do you wanna change the parameters? Right now we are using the standard B3/S23 rules. (Y/N): ')
+    if change_parameters == 'Y' or 'y':
+        survived = []
+        birthed = []
+        while True:
+            survive = input(
+                'What number of neighbours do you want a cell to have in order to surive? (name one number): ')
+            continuee = input('Do you want to add another number?: ')
+            if type(survive) != int and 0 <= int(survive) <= 8:
+                survived.append(int(survive))
+            if continuee != 'Y' and continuee != 'y':
+                break
 
-        :param width: integer representing the width of the world.
-        :param height: (optional) integer representing the height of the world. If left implicit, the value of ``width`` is used to create a square-shaped world.
-        """
-        self.width = width
-        if not height == -1:
-            self.height = height
-        else:
-            self.height = width
-        self.world = np.zeros((self.height, self.width), dtype=int)
+        while True:
+            birth = input(
+                'What number of neighbours do you want a dead cell to have in order to become alive? (name one number): ')
+            continuee = input('Do you want to add another number?: ')
+            if type(birth) != int and 0 <= int(birth) <= 8:
+                birthed.append(int(birth))
+            if continuee != 'Y' and continuee != 'y':
+                break
 
-    def get(self, x: int, y: int) -> int:
-        """
-        Returns the value on location ``(x, y)`` in the world.
+        sim.world.set_parameters(survived, birthed)
 
-        :param x: column-value of the location.
-        :param y: row-value of the location.
-        :return: value of location ``(x, y)`` in World.
-        """
-        if x < 0 or y < 0 or x >= self.width or y >= self.height:
-            return -1
-        return self.world[y][x]
-
-    def set(self, x: int, y: int, value:int = 1) -> None:
-        """
-        Sets the state of ``(x, y)`` to the given value.
-
-        :param x: column-value of the location.
-        :param y: row-value of the location.
-        :param value: (optional) value to set location ``(x, y)``; uses ``1`` otherwise.
-        """
-        if x < 0 or y < 0 or x >= self.width or y >= self.height:
-            return
-        self.world[y][x] = value
-
-    def get_neighbours(self, x: int, y:int) -> List[int]:
-        """
-        Returns a list of values for the 8 neighbours of location ``(x, y)``.
-
-        :param x: column-vale of the location.
-        :param y: row-value of the location.
-        :return: ``List`` of integers representing the values of the neighbours of ``(x, y)``.
-        """
-        neighbour_values = []
-        for nx in range(x-1,x+2):
-            for ny in range(y-1,y+2):
-                if not (nx is x and ny is y):
-                    neighbour_values.append(self.world[ny%self.height][nx%self.width])
-        return neighbour_values
-
-    def set_parameters(self, survive: int, birth: int):
-        self.survive = survive
-        self.birth = birth
-
-    def get_parameters(self):
-        return [self.survive, self.birth]
-
-    def __str__(self):
-        print('-'*self.width*4)
-        for row in self.world:
-            print('|', end=" ")
-            for column in row:
-                print(column, end=" | ")
-            print()
-            print('-'*self.width*4)
+    if VISUALISATION:
+        vis = Visualisation(sim)
+    else:
+        while True:
+            # Create new world and print to screen
+            print(sim.update())
+            # slow down simulation
+            time.sleep(0.5)
